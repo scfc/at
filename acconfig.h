12daa8d612bf796d9afbe3951e077c0e18541605
Cyril Brulebois <kibi@debian.org>
2009-08-11 20:50:01 +0200

    Import version 3.1.10
    Signed-off-by: Cyril Brulebois <kibi@debian.org>

---
+++ acconfig.h
@@ -0,0 +1,34 @@
+
+@BOTTOM@
+
+/* Define location of spool directories */
+
+#undef SPOOLDIR
+
+/* Define location for PID file */
+
+#undef PIDFILE
+
+/* Define mail command for sending */
+
+#undef SENDMAIL
+
+/* Where do we place out input directories? */
+
+#undef ATJOB_DIR
+
+/* Where do we spool our output? */
+
+#undef ATSPOOL_DIR
+
+/* What's the name of our PID file? */
+
+#undef PIDFILE
+
+/* Default queues for at and batch */
+
+#undef DEFAULT_AT_QUEUE
+
+#undef DEFAULT_BATCH_QUEUE
+
+#undef HAVE_ATTRIBUTE_NORETURN
